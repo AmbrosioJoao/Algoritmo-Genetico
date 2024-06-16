b5c25342b54eca9f344e1ab04a2c168bbc900262
AmbrosioJoao <ambrosiojoaomateus@gmail.com>
2024-06-15 22:18:35 -0300

    Fase inicial do projeto, definição de variáveis e abertura de arquivo de dados.

--- projeto/main/main.cpp
+++ projeto/main/main.cpp
@@ -2,9 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 
+//NOTAS:
+//Função ideal: f(x*) = -0,3 + 0,1x - 0,5x^2 + 0,4x^3
+
 int main()
 {
-    printf("Hello World");
-    printf("Commit de teste");
+    //VARIAVEIS
+    float a_ideal = -0.3; //conjunto ideal
+    float b_ideal = 0.1;
+    float c_ideal = -0.5;
+    float d_ideal = 0.4;
+
+    float a_variavel; //fatores que irão variar
+    float a_variavel;
+    float a_variavel;
+    float a_variavel;
+
+    int x; // faixa de valores
+
+    FILE* file; //ponteiro apontando para o arquivo
+
+    file = fopen_s(&file, "Dados.txt", "w"); // Abre o arquivo;
+
+
+    if (file == NULL) {
+        printf("Erro na abertura\n");
+        system("pause");
+        exit(1);
+    }
+
+
     return 0;
 }
