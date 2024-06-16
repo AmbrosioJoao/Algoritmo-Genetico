f1ceb8bace51b66a8e4ee75e97dda77297978b31
AmbrosioJoao <ambrosiojoaomateus@gmail.com>
2024-06-15 23:00:32 -0300

    Início do desenvolvimento do RMSE

--- projeto/main/main.cpp
+++ projeto/main/main.cpp
@@ -8,28 +8,46 @@
 int main()
 {
     //VARIAVEIS
-    float a_ideal = -0.3; //conjunto ideal
+    float a_ideal = -0.3; //Conjunto ideal
     float b_ideal = 0.1;
     float c_ideal = -0.5;
     float d_ideal = 0.4;
 
-    float a_variavel; //fatores que irão variar
-    float a_variavel;
-    float a_variavel;
-    float a_variavel;
+    float a_variavel= -10; //Fatores que irão variar
+    float b_variavel= -10;
+    float c_variavel= -10;
+    float d_variavel= -10;
 
-    int x; // faixa de valores
+    int n = 100; //Número de Amostras
+    float RMSE; //Erro médio quadrático
 
-    FILE* file; //ponteiro apontando para o arquivo
+    FILE *file; //Ponteiro apontando para o arquivo
+    errno_t err; //Variavel compativel com fopen_s
 
-    file = fopen_s(&file, "Dados.txt", "w"); // Abre o arquivo;
+    err = fopen_s(&file, "Dados.txt", "w"); // Abre o arquivo;
 
 
-    if (file == NULL) {
+    if (err != 0) {
         printf("Erro na abertura\n");
         system("pause");
         exit(1);
     }
+
+    //Essa variável x seria a faixa de valores do eixo.
+    for (int x = -50 ; x < 50; x++) {
+
+        //RMSE
+        RMSE = sqrt(pow(a_ideal + b_ideal * x + c_ideal * pow(x, 2) + d_ideal * pow(x, 3) -
+        a_variavel - b_variavel * x - c_variavel * pow(x, 2) - d_variavel * pow(x, 3), 2)/n);
+
+
+        for (int i = 0; i < 100; i++) { 
+
+        }
+
+
+    }
+
 
 
     return 0;
